34b1cd7b968a1b604b3bba01e86543f82e5bc6ff
GiriAditya14 <aditya.2004.giri@gmail.com>
2026-01-18 20:28:38 +0530

    add frontend url in cors

--- src/app.js
+++ src/app.js
@@ -6,9 +6,14 @@ const app = express()
 
 // configurations
 app.use(cors({
-    origin: process.env.CORS_ORIGIN ,
-    // origin: ["http://localhost:5173", ""],
+    origin: [
+        process.env.CORS_ORIGIN,
+        "https://videotube-frontend-opal.vercel.app",
+        "http://localhost:5173"
+    ],
     credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
 }))
 
 app.use(express.json({
