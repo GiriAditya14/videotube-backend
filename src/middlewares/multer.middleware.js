6a4b4f2492551703a08ba4570d3a5551bd9b99db
GiriAditya14 <aditya.2004.giri@gmail.com>
2026-01-18 20:27:06 +0530

    Use memory storage for Vercel serverless compatibility

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -1,14 +1,12 @@
 import multer from 'multer';
 
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, './public/temp')
-    },
-    filename: function (req, file, cb) {
-        cb(null, file.originalname)
-    }
-})
+// Use memory storage for Vercel serverless compatibility
+// Files will be stored in memory as buffers instead of disk
+const storage = multer.memoryStorage();
 
 export const upload = multer({
-    storage: storage 
+    storage: storage,
+    limits: {
+        fileSize: 100 * 1024 * 1024, // 100MB limit
+    },
 })
