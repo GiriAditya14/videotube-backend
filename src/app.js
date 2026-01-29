9e358ed3d9fbb439ca02d790c17b84a39f8830ad
GiriAditya14 <aditya.2004.giri@gmail.com>
2026-01-29 17:32:32 +0530

    fix: correct CORS origin configuration and update dotenv path

--- src/app.js
+++ src/app.js
@@ -6,8 +6,7 @@ const app = express()
 
 // configurations
 app.use(cors({
-    origin: process.env.CORS_ORIGIN ,
-    // origin: ["http://localhost:5173", ""],
+    origin: process.env.CORS_ORIGIN ? process.env.CORS_ORIGIN.split(',').map(origin => origin.trim()) : '*',
     credentials: true,
 }))
 
